5ae5857c0724daff707149fed0826630fd98299b
Jame Floc'h Le Carour <81566176+JameFLC@users.noreply.github.com>
2024-09-20 19:42:07 +0200

    Updates

--- _app/immutable/chunks/paths.CRXFKbiJ.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_k6ajbq)==null?void 0:s.base)??"/sveltepages";var a;const t=((a=globalThis.__sveltekit_k6ajbq)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.LfrnFlOH.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BrBX-ibN.js";export{t as start};
