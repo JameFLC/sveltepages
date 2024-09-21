2417cd8a3cd6ce7f1669487fff813d7f8b6bf3bb
Jame Floc'h Le Carour <81566176+JameFLC@users.noreply.github.com>
2024-09-21 18:32:53 +0200

    Changed description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Counter from '$lib/components/counter.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>This is a test to check different workflows with sveltekit and Github Pages / Actions</h1>
 <a href="{base}/blog">Go to blog</a>
 
 <h3>Here is a conter for you</h3>
